5559469fc5639bc74ce382d2f49139de9b9a4833
archit1604 <88767955+archit1604@users.noreply.github.com>
2024-07-04 16:12:41 +0530

    Update Primality_Test.cpp

--- Mathematics/Primality_Test.cpp
+++ Mathematics/Primality_Test.cpp
@@ -1,8 +1,8 @@
 bool isPrime(int N)
 {
     if(N<=1) return false;
-    for(int i=2;i<N;i++)
+    for(int i=2;i*i<=N;i++)
         if(N%i==0)
             return false;
-        return true;
+    return true;
 }
